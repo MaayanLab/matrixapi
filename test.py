d222c7e5ce42276ded4411b77105d6e227c7b9da
Alexander Lachmann <alexander.lachmann@gmail.com>
2021-02-08 16:39:08 -0500

    add coltop

--- test.py
+++ test.py
@@ -2,11 +2,50 @@ import json
 import requests
 import pandas as pd
 
-url = "http://localhost:5001/matrixapi/col"
+url = "http://localhost:5001/matrixapi/colid"
 
-payload = {"rowids": ["A1CF", "AAAS", "A2M"], "colids": ["ABHD1", "ABO", "doesnetexist"]}
+# this function will return the colids
+payload = {"id": "A1CF"}
 response = requests.post(url, json=payload)
 data = json.loads(response.text)
 print(data)
 
+url = "http://localhost:5001/matrixapi/col"
+
+# this function will return the full column specified
+payload = {"id": "A1CF"}
+response = requests.post(url, json=payload)
+data = json.loads(response.text)
+print(data)
+
+
+
+# extract sub matrix
+payload = {"rowids": ["A1CF", "AAAS", "A2M"], "colids": ["ABHD1", "ABO", "doesnetexist"]}
 response = requests.post("http://localhost:5001/matrixapi/slice", json=payload)
+data = json.loads(response.text)
+print(data)
+
+
+url = "http://localhost:5001/matrixapi/coltop"
+
+# this function will return the colids
+payload = {"id": "A1CF", "count": 20}
+response = requests.post(url, json=payload)
+data = json.loads(response.text)
+
+
+print(data)
+
+
+
+
+url = "https://maayanlab.cloud/matrixapi/coltop"
+data = ""
+# this function will return the colids
+payload = {"id": "SOX2", "count": 20}
+response = requests.post(url, json=payload)
+data = json.loads(response.text)
+
+print(data)
+
